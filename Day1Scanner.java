b3246a30893e09fbf2a98cc8672cecf2fa8c3526
Mr.Nikhil Vargude <115720074+nikhil7761@users.noreply.github.com>
2022-12-17 01:05:47 +0530

    Add files via upload

---
+++ Day1Scanner.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Day1Scanner {
+
+	public static void main(String[] args)
+	{
+		Scanner in=new Scanner(System.in); 
+
+		int x=in.nextInt();
+		double y=in.nextDouble();
+		byte k=in.nextByte();
+		
+		
+		System.out.println("Value x: "+x+ "y:"+y+ "k:"+k);
+ 
+     }
+	
+}
